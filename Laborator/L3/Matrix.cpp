c21b8cd6d818b02269c586a593d0a49fac67a7bb
LucaTheSorcerer <lucka.tudor@gmail.com>
2023-05-07 14:04:10 +0300

    It passes the tests, but I am still not sure whether it's circ or not

--- Laborator/L3/Matrix.cpp
+++ Laborator/L3/Matrix.cpp
@@ -3,7 +3,7 @@
 
 Matrix::Matrix(int nrLines, int nrCols) {
     if (nrLines <= 0 || nrCols <= 0)
-        throw std::exception();
+        throw std::out_of_range("Invalid dimensions");
 
     lines = nrLines;
     columns = nrCols;
@@ -26,6 +26,14 @@ Matrix::Matrix(int nrLines, int nrCols) {
     for (int i = 0; i < nrCols; ++i)
         columnHead[i] = lastColumn[i] = -1;
 
+    lastLine[nrLines - 1] = (nrLines - 1) * columns;
+    nodes[lastLine[nrLines - 1]].nextLine = lineHead[0];
+
+    lastColumn[columns - 1] = columns - 1;
+    nodes[lastColumn[columns - 1]].nextColumn = columnHead[0];
+
+
+
     for (int i = 0; i < capacity - 1; ++i) {
         nodes[i].nextLine = i + 1;
         nodes[i].nextColumn = i + 1;
@@ -33,6 +41,40 @@ Matrix::Matrix(int nrLines, int nrCols) {
 
     nodes[capacity - 1].nextLine = -1;
     nodes[capacity - 1].nextColumn = -1;
+
+    for (int i = 0; i < lines; ++i) {
+        if (lastLine[i] != -1) {
+            nodes[lastLine[i]].nextLine = i * columns;
+            nodes[i * columns].nextLine = lineHead[i];
+        }
+        if (lineHead[i] != -1) {
+            nodes[i * columns + columns - 1].nextLine = lineHead[i];
+            nodes[lineHead[i]].nextLine = i * columns + columns - 1;
+        }
+    }
+    for (int i = 0; i < columns; ++i) {
+        if (lastColumn[i] != -1) {
+            nodes[lastColumn[i]].nextColumn = i;
+            nodes[i].nextColumn = columnHead[i];
+        }
+        if (columnHead[i] != -1) {
+            nodes[(lines - 1) * columns + i].nextColumn = columnHead[i];
+            nodes[columnHead[i]].nextColumn = (lines - 1) * columns + i;
+        }
+    }
+
+
+    // create a circular connection between the last and the first element in each line and column
+    for (int i = 0; i < lines; i++) {
+        if (lastLine[i] != -1) {
+            nodes[lastLine[i]].nextLine = lineHead[i];
+        }
+    }
+    for (int i = 0; i < columns; i++) {
+        if (lastColumn[i] != -1) {
+            nodes[lastColumn[i]].nextColumn = columnHead[i];
+        }
+    }
 }
 
 int Matrix::nrLines() const {
@@ -58,73 +100,16 @@ TElem Matrix::element(int i, int j) const {
     return NULL_TELEM;
 }
 
-//TElem Matrix::modify(int i, int j, TElem e) {
-//    if (i < 0 || i >= lines || j < 0 || j >= columns)
-//        throw std::exception();
-//
-//    int index = lineHead[i];
-//    int prevIndex = -1;
-//
-//    while (index != -1) {
-//        if (nodes[index].column == j) {
-//            TElem oldValue = nodes[index].value;
-//            nodes[index].value = e;
-//            return oldValue;
-//        }
-//        prevIndex = index;
-//        index = nodes[index].nextLine;
-//    }
-//
-//    if (e == NULL_TELEM)
-//        return NULL_TELEM;
-//
-//    if (firstEmpty == -1) {
-//        automaticResize();
-//    }
-//
-//    int newNodeIndex = firstEmpty;
-//    firstEmpty = nodes[firstEmpty].nextLine;
-//
-//    nodes[newNodeIndex].line = i;
-//    nodes[newNodeIndex].column = j;
-//    nodes[newNodeIndex].value = e;
-//
-//    if (lineHead[i] == -1 || nodes[lineHead[i]].column > j) {
-//        nodes[newNodeIndex].nextLine = lineHead[i];
-//        lineHead[i] = newNodeIndex;
-//    } else {
-//        nodes[newNodeIndex].nextLine = nodes[prevIndex].nextLine;
-//        nodes[prevIndex].nextLine = newNodeIndex;
-//    }
-//
-//    int colIndex = columnHead[j];
-//    int prevColIndex = -1;
-//
-//    while (colIndex != -1 && nodes[colIndex].line < i) {
-//        prevColIndex = colIndex;
-//        colIndex = nodes[colIndex].nextColumn;
-//    }
-//
-//    if (prevColIndex == -1) {
-//        nodes[newNodeIndex].nextColumn = columnHead[j];
-//        columnHead[j] = newNodeIndex;
-//    } else {
-//        nodes[newNodeIndex].nextColumn = nodes[prevColIndex].nextColumn;
-//        nodes[prevColIndex].nextColumn = newNodeIndex;
-//    }
-//
-//    size++;
-//
-//    return NULL_TELEM;
-//}
-
 
 TElem Matrix::modify(int i, int j, TElem e) {
     if (i < 0 || i >= lines || j < 0 || j >= columns)
         throw std::exception();
 
     int index = lineHead[i];
-    int prevIndex = -1;
+//    int prevIndex = -1;
+// initialize prevIndex to the last node of the line
+    int prevIndex = lastLine[i];
+
 
     while (index != -1) {
         if (nodes[index].column == j) {
@@ -157,6 +142,7 @@ TElem Matrix::modify(int i, int j, TElem e) {
         prevIndex = index;
         index = nodes[index].nextLine;
     }
+
 
     if (e == NULL_TELEM)
         return NULL_TELEM;
@@ -196,6 +182,9 @@ TElem Matrix::modify(int i, int j, TElem e) {
     }
 
     size++;
+
+//    lastLine[i] = index;
+//    lastColumn[j] = colIndex;
 
     return NULL_TELEM;
 
